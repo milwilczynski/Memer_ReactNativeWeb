d757966bb0a6f6422167daa3913578fce24e556a
Miłosz Wilczyński <mwilcz08@gmail.com>
2020-06-19 15:26:16 +0200

    CardMaker Done

--- src/screens/Random.js
+++ src/screens/Random.js
@@ -5,6 +5,7 @@ import {UserContext} from "../modules/auth/UserContext";
 import Score from "../components/smart/Score";
 import ErrorHandler from "../modules/errors/ErrorHandler";
 import Card from "react-bootstrap/Card";
+import CardMaker from "../components/smart/CardMaker";
 
 export function Random() {
     const {user} = useContext(UserContext);
@@ -46,45 +47,7 @@ export function Random() {
             )
             let card = [];
             card.push(
-                <Card key={res.name} style={{
-                    marginTop: '2%',
-                    width: '100%',
-                    height: '80%',
-                    borderStyle: 'solid',
-                    borderWidth: '1px',
-                    borderColor: '#ffd31d',
-                    padding: "0 10px 20px 10px",
-                }}>
-                    <Card.Body style={{borderRadius: '5%'}}>
-                        <View
-                            style={{
-                                flex: 0.5,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                            }}>
-                            <Text style={{fontSize: '110%', color: 'grey'}}>
-                                {res.title}
-                            </Text>
-                        </View>
-                        <Card.Img
-                            style={{width: '100%', height: "95%", borderRadius: '1%'}}
-                            src={
-                                'http://localhost:8080/upload/static/images/' + res.name
-                            }/>
-                        <View style={{flexDirection: 'row', flex: 1, height: '5%'}}>
-                            <View style={{flexDirection: 'row', flex: 1, marginTop: '5px', marginBottom: '5px'}}>
-                                {tags}
-                            </View>
-                            <View style={{flexGrow: 'flex-end'}}>
-                                <Score
-                                    name={res.name}
-                                    token={user}
-                                    score={res.points}
-                                />
-                            </View>
-                        </View>
-                    </Card.Body>
-                </Card>
+                <CardMaker imageParam={res} token={user}/>
             )
             setPost(card);
         } catch (err) {
